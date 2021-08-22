fa149b65f0fc95d06de4f16a0318773bd61ab85a
siwakorn21 <siwakornboonmun@gmail.com>
2021-08-22 18:49:47 +0700

    first commit

---
+++ assignment-2/cpu_test.py
@@ -0,0 +1,15 @@
+import  time
+
+# TESTING
+n = 50000
+result = []
+
+for i in range(n):
+    result.append(time.time_ns())
+
+# WRITE FILE
+f = open('result-4.txt', 'w')
+f.write(f'{0}, {0 / 10**9} \n')
+for i in range(1, len(result)):
+    f.write(f'{i}, {(result[i] - result[0]) / (10 ** 9)} \n')
+f.close()
